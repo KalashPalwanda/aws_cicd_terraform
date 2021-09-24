21324dd82fbe03e2df57f2a25cb2fa5b5310bc7a
kalashpalwanda <kalashp69@gmail.com>
2021-09-24 06:29:56 +0000

    commit

--- pipline.tf
+++ pipline.tf
@@ -11,22 +11,29 @@ resource "aws_codepipeline" "static_web_pipeline" {
   }
 
   stage {
-        name = "Source"
-        action{
-            name = "Source"
-            category = "Source"
-            owner = "AWS"
-            provider = "CodeStarSourceConnection"
-            version = "1"
-            output_artifacts = ["SourceArtifact"]
-            configuration = {
-                FullRepositoryId = "KalashPalwanda/static-web-example"
-                BranchName   = "main"
-                ConnectionArn = var.codestar_connector_credentials
-                OutputArtifactFormat = "CODE_ZIP"
-            }
-        }
+    name = "Source"
+
+    action {
+      category = "Source"
+      configuration = {
+        "Branch"               = var.repository_branch
+        "Owner"                = var.repository_owner
+        "PollForSourceChanges" = "false"
+        "Repo"                 = var.repository_name
+        OAuthToken             = var.github_token
+      }
+
+      input_artifacts = []
+      name            = "Source"
+      output_artifacts = [
+        "SourceArtifact",
+      ]
+      owner     = "ThirdParty"
+      provider  = "GitHub"
+      run_order = 1
+      version   = "1"
     }
+  }
 
   stage {
     name = "Build"
