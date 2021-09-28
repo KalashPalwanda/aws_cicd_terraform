687980fc07ed8dc0ce957159be7eff76d238aff0
kalashpalwanda <kalashp69@gmail.com>
2021-09-28 12:44:36 +0000

    commit

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ provider "aws" {
   region  = var.region
   version = "~> 3.15.0"
 }
-resource "aws_instance" "us-west-2" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
-}
-
-
 provider "github" {
   token   = var.github_token
   owner   = var.repository_owner
